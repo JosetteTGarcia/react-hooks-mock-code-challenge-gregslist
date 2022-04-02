3ee15c306b02ad736fc479520bacbe2354d756ec
Josette Garcia <josettetgarcia@gmail.com>
2022-04-02 16:32:19 -0500

    commit tree 24ce17bf0128008200407f15fde2bd3e8ecda18c

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({listing, onDeleteClick}) {
+const {id, image, description, location} = listing
+const [isStarred, setIsStarred] = useState(false)
+
+function handleStarClick(){
+  setIsStarred(true)
+}
+
+function handleDeleteCLick(){
+  onDeleteClick(id);
+}
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+      <div className="details" onClick={() => handleStarClick()}>
+        {isStarred ? (
+          <button className="emoji-button favorite active">★</button> 
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={handleDeleteCLick}>🗑</button>
       </div>
     </li>
   );
 }
 
 export default ListingCard;
+ 
